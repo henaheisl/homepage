435823da7a1afe87ef527348e23a4a37db240806
michael <m.deimel@miceportal.com>
2019-10-25 11:17:01 +0200

    Updates

---
+++ precache-manifest.73a9de49cea3881a8054e9f6ae25378a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88070144ff851c66eacf44a266ffe9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae25b7d2270b125a6a3b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b03b411756dce0a70f7a",
+    "url": "/static/css/main.001c61ba.chunk.css"
+  },
+  {
+    "revision": "ae25b7d2270b125a6a3b",
+    "url": "/static/js/2.b3477cfb.chunk.js"
+  },
+  {
+    "revision": "b03b411756dce0a70f7a",
+    "url": "/static/js/main.4816abe8.chunk.js"
+  },
+  {
+    "revision": "a573d47fd9b9e29d98de",
+    "url": "/static/js/runtime-main.9ec1eb5f.js"
+  }
+]);
